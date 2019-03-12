40a5d9cb555ee556325e7c18b7d0078cbe3adac0
Mukesh Maurya <mbmaurya.maurya@gmail.com>
2019-03-12 19:47:26 +0530

    Added registration functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +12,7 @@ import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { LoginComponent } from './authenticate/login/login.component';
 import { RegisterComponent } from './authenticate/register/register.component';
 import { ForgotpasswordComponent } from './authenticate/forgotpassword/forgotpassword.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,9 +25,10 @@ import { ForgotpasswordComponent } from './authenticate/forgotpassword/forgotpas
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/authenticate/register/register.component.ts
+++ src/app/authenticate/register/register.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import { environment } from '../../../environments/environment'
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  password: string;
+  gender: string;
+  dob: Date;
+}
 
 @Component({
   selector: 'app-register',
@@ -7,6 +21,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
 
   registrationForm = this.fb.group({
     firstName: ['', Validators.required],
@@ -23,12 +38,28 @@ export class RegisterComponent implements OnInit {
   get password() { return this.registrationForm.get('password')}
   get dob() { return this.registrationForm.get('dob')}
   
+  private usersCollection: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
 
-  registerUser() {
-    console.log('Sign Up pressed');
-  }
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) {
+    this.usersCollection = afs.collection<User>('users');
+    this.users = this.usersCollection.valueChanges();
+   }
 
-  constructor(private fb: FormBuilder) { }
+   registerUser(user: User) {
+     console.log('Signup Pressed');
+     console.log(user);
+    //  console.log(this.registrationForm.value);
+    if(this.usersCollection.add(this.registrationForm.value)) {
+      firebase.auth().createUserWithEmailAndPassword(this.registrationForm.value.emailAddress, this.registrationForm.value.password)
+        .catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(`{{errorCode}}, {{errorMessage}}`);
+      });
+    }
+     
+   }
 
   ngOnInit() {
   }
